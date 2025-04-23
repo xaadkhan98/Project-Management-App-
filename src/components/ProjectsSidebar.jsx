c63e625bb64d0450fcf647301c70c7e0a9435807
xaadkhan98 <xaadkhan98@gmail.com>
2025-04-23 12:47:46 +0500

    Project Management App

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function ProjectsSidebar() {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl uppercase text-stone-50">
+        Your Projects
+      </h2>
+      <button className="px-4 py-2 rounded-md text-xs md:text-base bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+        + Add Projects
+      </button>
+      <div>
+        <ul></ul>
+      </div>
+    </aside>
+  );
+}
